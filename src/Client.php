93545dbe3ad943744ff71eae6edf86c56cf73d93
İbrahim Ş. Örencik <o@yedincisenol.com>
2019-09-04 23:18:50 +0300

    Update Client.php
    Product model added

--- src/Client.php
+++ src/Client.php
@@ -17,6 +17,7 @@ use yedincisenol\Parasut\Models\PurchaseBill;
 use yedincisenol\Parasut\Models\SaleInvoice;
 use yedincisenol\Parasut\Models\Tag;
 use yedincisenol\Parasut\Models\Trackable;
+use yedincisenol\Parasut\Models\Trackable;
 
 class Client
 {
@@ -361,6 +362,15 @@ class Client
     public function saleInvoice()
     {
         return new SaleInvoice($this);
+    }
+    
+     /**
+     * Get Product model
+     * @return Product
+     */
+    public function product()
+    {
+        return new Product($this);
     }
 
     /**
